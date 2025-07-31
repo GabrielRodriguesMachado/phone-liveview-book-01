fe23b71e8cdf54655d875242f28b841a46586edd
Gabriel Rodrigues Machado <gabrm123@gmail.com>
2025-07-30 21:34:38 -0300

    Add session ID and user email to WrongLive view

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -2,8 +2,14 @@ defmodule PentoWeb.WrongLive do
   require Logger
   use PentoWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+  def mount(_params, session, socket) do
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       time: time(),
+       session_id: session["live_socket_id"]
+     )}
   end
 
   def handle_params(_params, _url, socket) do
@@ -32,6 +38,11 @@ defmodule PentoWeb.WrongLive do
             </.link>
           <% end %>
         </h2>
+        <br />
+        <pre>
+          {@current_user.email}
+          {@session_id}
+        </pre>
         """
 
       :win ->
--- pento/lib/pento_web/router.ex
+++ pento/lib/pento_web/router.ex
@@ -21,8 +21,6 @@ defmodule PentoWeb.Router do
     pipe_through(:browser)
 
     get("/", PageController, :home)
-    live("/guess", WrongLive, :play)
-    live("/guess/win", WrongLive, :win)
   end
 
   # Other scopes may use custom stacks.
@@ -67,9 +65,13 @@ defmodule PentoWeb.Router do
     pipe_through([:browser, :require_authenticated_user])
 
     live_session :require_authenticated_user,
+      root_layout: {PentoWeb.Layouts, :root},
       on_mount: [{PentoWeb.UserAuth, :ensure_authenticated}] do
       live("/users/settings", UserSettingsLive, :edit)
       live("/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email)
+
+      live("/guess", WrongLive, :play)
+      live("/guess/win", WrongLive, :win)
     end
   end
 
